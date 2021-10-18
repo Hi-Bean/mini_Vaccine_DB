5a391a86b26c4aaf0e56b7dbae00c995658ff671
2hyerin <88357058+2hyerin@users.noreply.github.com>
2021-10-18 12:37:53 +0900

    Add files via upload
    2021.10.18 hr

---
+++ miniProject_Vaccine/miniProject_Vaccine/frmAppointment.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace miniProject_Vaccine
+{
+    partial class frmAppointment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.cbVaccine = new System.Windows.Forms.ComboBox();
+            this.dtDate = new System.Windows.Forms.DateTimePicker();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbResidenRegisNum = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbPhoneNum = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbArea = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rbFemale
+            // 
+            this.rbFemale.AutoSize = true;
+            this.rbFemale.Location = new System.Drawing.Point(232, 249);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(58, 19);
+            this.rbFemale.TabIndex = 36;
+            this.rbFemale.TabStop = true;
+            this.rbFemale.Text = "여자";
+            this.rbFemale.UseVisualStyleBackColor = true;
+            // 
+            // rbMale
+            // 
+            this.rbMale.AutoSize = true;
+            this.rbMale.Location = new System.Drawing.Point(168, 249);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(58, 19);
+            this.rbMale.TabIndex = 35;
+            this.rbMale.TabStop = true;
+            this.rbMale.Text = "남자";
+            this.rbMale.UseVisualStyleBackColor = true;
+            // 
+            // cbVaccine
+            // 
+            this.cbVaccine.FormattingEnabled = true;
+            this.cbVaccine.Location = new System.Drawing.Point(168, 89);
+            this.cbVaccine.Name = "cbVaccine";
+            this.cbVaccine.Size = new System.Drawing.Size(194, 23);
+            this.cbVaccine.TabIndex = 34;
+            // 
+            // dtDate
+            // 
+            this.dtDate.Location = new System.Drawing.Point(168, 120);
+            this.dtDate.Name = "dtDate";
+            this.dtDate.Size = new System.Drawing.Size(194, 25);
+            this.dtDate.TabIndex = 33;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(197, 310);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(106, 34);
+            this.btnCancel.TabIndex = 32;
+            this.btnCancel.Text = "취소하기";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(73, 310);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(106, 34);
+            this.btnOk.TabIndex = 31;
+            this.btnOk.Text = "예약하기";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(109, 123);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 15);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "Date";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(91, 249);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 15);
+            this.label5.TabIndex = 29;
+            this.label5.Text = "Gender";
+            // 
+            // tbResidenRegisNum
+            // 
+            this.tbResidenRegisNum.Location = new System.Drawing.Point(168, 215);
+            this.tbResidenRegisNum.Name = "tbResidenRegisNum";
+            this.tbResidenRegisNum.Size = new System.Drawing.Size(194, 25);
+            this.tbResidenRegisNum.TabIndex = 28;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 218);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(135, 15);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "Residen Regis Num";
+            // 
+            // tbPhoneNum
+            // 
+            this.tbPhoneNum.Location = new System.Drawing.Point(168, 184);
+            this.tbPhoneNum.Name = "tbPhoneNum";
+            this.tbPhoneNum.Size = new System.Drawing.Size(194, 25);
+            this.tbPhoneNum.TabIndex = 26;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 15);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Phone Num";
+            // 
+            // tbArea
+            // 
+            this.tbArea.Location = new System.Drawing.Point(168, 151);
+            this.tbArea.Name = "tbArea";
+            this.tbArea.Size = new System.Drawing.Size(194, 25);
+            this.tbArea.TabIndex = 24;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(109, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 15);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Area";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 15);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Vaccine";
+            // 
+            // tbId
+            // 
+            this.tbId.Location = new System.Drawing.Point(168, 58);
+            this.tbId.Name = "tbId";
+            this.tbId.Size = new System.Drawing.Size(194, 25);
+            this.tbId.TabIndex = 21;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(127, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 15);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Id";
+            // 
+            // Appointment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 389);
+            this.Controls.Add(this.rbFemale);
+            this.Controls.Add(this.rbMale);
+            this.Controls.Add(this.cbVaccine);
+            this.Controls.Add(this.dtDate);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbResidenRegisNum);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbPhoneNum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbArea);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbId);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Appointment";
+            this.Text = "Appointment";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbFemale;
+        private System.Windows.Forms.RadioButton rbMale;
+        private System.Windows.Forms.ComboBox cbVaccine;
+        private System.Windows.Forms.DateTimePicker dtDate;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        public System.Windows.Forms.TextBox tbResidenRegisNum;
+        private System.Windows.Forms.Label label6;
+        public System.Windows.Forms.TextBox tbPhoneNum;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.TextBox tbArea;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.TextBox tbId;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ miniProject_Vaccine/miniProject_Vaccine/frmMain.Designer.cs
@@ -0,0 +1,341 @@
+﻿
+namespace miniProject_Vaccine
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.병원열기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.연결하기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbHospiName = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbVaccineM = new System.Windows.Forms.Label();
+            this.lbVaccineH = new System.Windows.Forms.Label();
+            this.lbVaccineY = new System.Windows.Forms.Label();
+            this.lbVaccineA = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.btnAppointment = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox2);
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.btnAppointment);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Size = new System.Drawing.Size(816, 465);
+            this.splitContainer1.SplitterDistance = 524;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.textBox1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.textBox2);
+            this.splitContainer2.Size = new System.Drawing.Size(524, 465);
+            this.splitContainer2.SplitterDistance = 308;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(0, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(524, 301);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.ContextMenuStrip = this.contextMenuStrip1;
+            this.textBox2.Location = new System.Drawing.Point(-1, -2);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(524, 155);
+            this.textBox2.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.병원열기ToolStripMenuItem,
+            this.연결하기ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(139, 52);
+            // 
+            // 병원열기ToolStripMenuItem
+            // 
+            this.병원열기ToolStripMenuItem.Name = "병원열기ToolStripMenuItem";
+            this.병원열기ToolStripMenuItem.Size = new System.Drawing.Size(138, 24);
+            this.병원열기ToolStripMenuItem.Text = "병원열기";
+            // 
+            // 연결하기ToolStripMenuItem
+            // 
+            this.연결하기ToolStripMenuItem.Name = "연결하기ToolStripMenuItem";
+            this.연결하기ToolStripMenuItem.Size = new System.Drawing.Size(138, 24);
+            this.연결하기ToolStripMenuItem.Text = "연결하기";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.lbHospiName);
+            this.groupBox2.Location = new System.Drawing.Point(40, 37);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(204, 124);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "병원 정보";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "병원 전화 번호";
+            // 
+            // lbHospiName
+            // 
+            this.lbHospiName.AutoSize = true;
+            this.lbHospiName.Location = new System.Drawing.Point(23, 39);
+            this.lbHospiName.Name = "lbHospiName";
+            this.lbHospiName.Size = new System.Drawing.Size(72, 15);
+            this.lbHospiName.TabIndex = 0;
+            this.lbHospiName.Text = "병원 이름";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbVaccineM);
+            this.groupBox1.Controls.Add(this.lbVaccineH);
+            this.groupBox1.Controls.Add(this.lbVaccineY);
+            this.groupBox1.Controls.Add(this.lbVaccineA);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.Label2);
+            this.groupBox1.Location = new System.Drawing.Point(40, 188);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(205, 170);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "백신 종류/수량";
+            // 
+            // lbVaccineM
+            // 
+            this.lbVaccineM.AutoSize = true;
+            this.lbVaccineM.Location = new System.Drawing.Point(94, 136);
+            this.lbVaccineM.Name = "lbVaccineM";
+            this.lbVaccineM.Size = new System.Drawing.Size(15, 15);
+            this.lbVaccineM.TabIndex = 2;
+            this.lbVaccineM.Text = "0";
+            // 
+            // lbVaccineH
+            // 
+            this.lbVaccineH.AutoSize = true;
+            this.lbVaccineH.Location = new System.Drawing.Point(94, 102);
+            this.lbVaccineH.Name = "lbVaccineH";
+            this.lbVaccineH.Size = new System.Drawing.Size(15, 15);
+            this.lbVaccineH.TabIndex = 2;
+            this.lbVaccineH.Text = "0";
+            // 
+            // lbVaccineY
+            // 
+            this.lbVaccineY.AutoSize = true;
+            this.lbVaccineY.Location = new System.Drawing.Point(94, 71);
+            this.lbVaccineY.Name = "lbVaccineY";
+            this.lbVaccineY.Size = new System.Drawing.Size(15, 15);
+            this.lbVaccineY.TabIndex = 2;
+            this.lbVaccineY.Text = "0";
+            // 
+            // lbVaccineA
+            // 
+            this.lbVaccineA.AutoSize = true;
+            this.lbVaccineA.Location = new System.Drawing.Point(94, 41);
+            this.lbVaccineA.Name = "lbVaccineA";
+            this.lbVaccineA.Size = new System.Drawing.Size(15, 15);
+            this.lbVaccineA.TabIndex = 2;
+            this.lbVaccineA.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(23, 136);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 15);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "모더나";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 15);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "화이자";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "얀센";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(23, 41);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(25, 15);
+            this.Label2.TabIndex = 2;
+            this.Label2.Text = "AZ";
+            // 
+            // btnAppointment
+            // 
+            this.btnAppointment.Location = new System.Drawing.Point(155, 388);
+            this.btnAppointment.Name = "btnAppointment";
+            this.btnAppointment.Size = new System.Drawing.Size(90, 28);
+            this.btnAppointment.TabIndex = 1;
+            this.btnAppointment.Text = "예약하기";
+            this.btnAppointment.UseVisualStyleBackColor = true;
+            this.btnAppointment.Click += new System.EventHandler(this.btnAppointment_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "<Info>";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 465);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnAppointment;
+        protected System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 병원열기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 연결하기ToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lbHospiName;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbVaccineM;
+        private System.Windows.Forms.Label lbVaccineH;
+        private System.Windows.Forms.Label lbVaccineY;
+        private System.Windows.Forms.Label lbVaccineA;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
